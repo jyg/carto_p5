ac3e5e254128f0678fd8d2645ad8d09734c5a34e
Jean-Yves Gratius <jyg@gumo.fr>
2026-05-05 15:17:26 +0200

    Update my_script.js

--- webpatch/my_script/my_script.js
+++ webpatch/my_script/my_script.js
@@ -35,7 +35,7 @@ let topOffset = 0;
 let presetButtons = [];
 
 // list of presets. For each name corresponds a .csv file in assets folder
-let presetList = ['Intro','Tuto','groupeA', 'groupeB','CarteBlanche','acqua', 'tapMardi'];
+let presetList = ['Intro','Tuto','groupeA', 'groupeB','CarteBlanche','acqua', 'acqua'];
 
 let currentPreset = -1;
 
